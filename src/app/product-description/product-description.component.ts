21b400f49d92ab85ce4e315d90bda3c0d20c7357
cephas <kosisochukwuonwuliri@gmail.com>
2021-12-12 22:47:05 +0100

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ProductService } from '../product.service';
-import { Album } from 'app/album';
-import { ALBUM } from '.../album';
+
+import { Album } from '.../album';
 
 @Component({
   selector: 'app-product-description',
